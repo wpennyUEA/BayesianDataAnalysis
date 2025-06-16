01ee6ca12526b05f0378924a2d941c21cb3fade7
Will Penny <W.Penny@uea.ac.uk>
2025-06-16 10:54:35 +0100

    Add files via upload

---
+++ Contents.m
@@ -0,0 +1,39 @@
+%
+% BAYESIAN DATA ANALYSIS TOOLBOX
+% 
+% Version 2.0, June 2025
+%
+% Will Penny, School of Psychology, University of East Anglia
+%
+% ----------------------------------------------------------------------------------------
+% autoregressive                    Autoregressive Time Series Modelling
+% linear-regression                 Bayesian Univariate Linear Regression, ANOVAs, T-Tests
+% misc                              Miscellaneous Routines
+% mixed-effects                     Mixed Effects Inference
+% mixture-canonical-correlates      Mixtures of Canonical Correlate Analysers 
+% mixture-gaussians                 Mixtures of Gaussians
+% multivariate-linear               Multivariate Linear Models
+% nonlinear-components              Neighbourhood Component Analysis
+% principal-components              Principal Component Analysis
+% robust-glm                        Robust General Linear Models
+% softmax-regression                Softmax Regression
+% spectral-analysis                 Spectral Estimation
+% spm-code                          Some SPM12 functions called by the above
+%
+% ----------------------------------------------------------------------------------------
+% Some of this software was previously available in SPM12.
+% https://www.fil.ion.ucl.ac.uk/spm/
+%
+% It also includes many more recent edits, developments and several contributions
+% from third parties with wrappers to their original code. 
+%
+% Run bda_setpath.m and then you're off !
+% Read Contents.m in relevant subdirectory and run demos
+%
+% Third party contributions include:
+%
+% MCCA code by J Viinikanoja, University of Helsinki, 2010. The original
+% MCCA code also uses Tom Minka's Lightspeed Toolbox, Microsoft, 2017.
+% The SPM12 functions are written by various authors at UCL including
+% Karl Friston, Guillaume Flandin, Andrew Holmes and Jean-Baptiste Poline.
+%
---
+++ bda_setpath.m
@@ -0,0 +1,11 @@
+
+% Bayesian Data Analysis Toolbox version 2.0
+% Will Penny, School of Psychology, UEA, 2023.
+
+% Add subdirectories to path
+addpath(genpath(pwd));
+
+
+
+
+
